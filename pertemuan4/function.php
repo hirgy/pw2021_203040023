c10fbf526412df86353a84edb667161c00db36cc
hirgy <hirgy9@gmail.com>
2021-02-25 13:37:11 +0700

    menyelesaikan video pertemuan 4

---
+++ pertemuan4/function.php
@@ -0,0 +1,44 @@
+<?php
+function salam($waktu = "Datang", $nama = "Admin") {
+    return "Selamat $waktu, $nama!";
+}
+
+
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Latihan Function</title>
+</head>
+<body>
+    <h1>Selamat Datang, Administrator!</h1>
+</body>
+</html> -->
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Latihan Function</title>
+</head>
+<body>
+    <h1><?= salam("Pagi", "Hirgy"); ?></h1>
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Latihan Function</title>
+</head>
+<body>
+    <h1><?= salam("Pagi"); ?></h1>
+</body>
+</html>
+
+/*
+    Nama : Muhammad Rhyamizard Hirgy
+    NRP : 203040023
+    Kelas A
+    Pertemuan ke 4
+*/
